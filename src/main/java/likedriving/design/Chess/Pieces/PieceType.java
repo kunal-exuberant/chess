48eeb242845ae0e47524a55e9dad8a321b5dd2a2
Kunal Singh <kunalsingh.k@kunal-exuberant.local>
2019-06-28 14:29:59 +0530

    new repo for chess

---
+++ src/main/java/likedriving/design/Chess/Pieces/PieceType.java
@@ -0,0 +1,10 @@
+package likedriving.design.Chess.Pieces;
+
+public enum PieceType {
+    KING,
+    QUEEN,
+    KNIGHT,
+    ROOK,
+    BISHOP,
+    PAWN
+}
---
+++ src/main/java/likedriving/design/Chess/TerminalCondition.java
@@ -0,0 +1,9 @@
+package likedriving.design.Chess;
+
+public enum TerminalCondition {
+    CHECK_MATE,
+    DRAW,
+    STALE_MATE,
+    INSUFFICIENT_MATERIAL,
+    THREE_SAME_MOVES
+}
